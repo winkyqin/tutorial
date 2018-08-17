e04daaeed5e31eae21f06befde6d6a529bb49a5c
Winky_Wei <winkyqin@163.com>
2018-08-17 18:05:43 +0800

    Initial commit

---
+++ tutorial/spiders/quotes_spider.py
@@ -0,0 +1,35 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+
+    name = "quotes"
+
+    def start_requests(self):
+
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        # follow links to author pages
+        for href in response.css('.author + a::attr(href)'):
+            yield response.follow(href, self.parse_author)
+
+        # follow pagination links
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, self.parse)
+
+    def parse_author(self, response):
+
+        def extract_with_css(query):
+            return response.css(query).extract_first().strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
